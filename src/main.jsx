1897b2859a38acb38904089fbb0b57d19d3d2825
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-30 14:58:35 -0400

    added colormodescript

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App"
-import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import App from "./components/App";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme.js"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
