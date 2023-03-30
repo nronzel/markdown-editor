f020af3f7a775c9a417bda3451f9a5b49d2c0ca6
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-30 16:01:52 -0400

    updated homepage

--- src/components/HomepageHero.jsx
+++ src/components/HomepageHero.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, Flex, Input, Text } from "@chakra-ui/react";
+import { Button, Divider, Flex, Input, Text } from "@chakra-ui/react";
 import React, { useState, useContext } from "react";
 import { auth, googleProvider } from "../config/firebase";
 import {
@@ -13,6 +13,7 @@ const HomepageHero = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const signIn = async () => {
     try {
@@ -20,10 +21,10 @@ const HomepageHero = () => {
         logOut();
       } else {
         await createUserWithEmailAndPassword(auth, email, password);
-        // useNavigate("/mainapp")
+        // navigate("/mainapp")
       }
     } catch (err) {
-      console.error(err);
+      alert(err.message);
     }
   };
 
@@ -31,42 +32,48 @@ const HomepageHero = () => {
     try {
       await signInWithPopup(auth, googleProvider);
     } catch (err) {
-      console.error(err);
+      alert(err.message);
     }
   };
 
   const logOut = async () => {
     try {
       await signOut(auth);
-        // useNavigate("/")
+      alert("Logged Out Successfully");
+      navigate("/");
     } catch (err) {
-      console.error(err);
+      alert(err.message);
     }
   };
 
   return (
     <Flex justifyContent="center" gap={7} h="450px">
-      {currentUser && <Text>Signed in as {email}</Text>}
       <Flex alignItems="center">
         <Text>Edit and save Markdown files right in your browser!</Text>
       </Flex>
       <Divider orientation="vertical" />
       <Flex direction="column" gap={5}>
-        <Text textAlign="center">Sign In</Text>
-        <Input
-          type="email"
-          placeholder="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          placeholder="password"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button onClick={signIn}>
-            {currentUser ? "Log Out" : "Sign In"}
-        </Button>
-        <Button onClick={signInWithGoogle}>Sign In With Google</Button>
+        {currentUser ? (
+          <Text>Welcome!</Text>
+        ) : (
+          <>
+            <Text textAlign="center">Sign In</Text>
+            <Input
+              type="email"
+              placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signIn}>
+              {currentUser ? "Log Out" : "Sign In"}
+            </Button>
+            <Button onClick={signInWithGoogle}>Sign In With Google</Button>
+          </>
+        )}
       </Flex>
     </Flex>
   );
