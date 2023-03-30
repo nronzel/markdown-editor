08cc4e570c94d22af9da2f11550f2249f6e8b04c
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-30 14:59:11 -0400

    integrated firebase auth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
-import '../App.css'
+import Home from "../pages/home.jsx";
+import React from "react";
+import { Flex } from "@chakra-ui/react";
+import { AuthProvider } from "../config/AuthProvider.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <AuthProvider>
+      <Flex direction="column">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" index element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </Flex>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
