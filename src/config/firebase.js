6be6076b3dd629492b86670dc8063e5ff9d493cc
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-30 16:00:52 -0400

    export default app

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -13,4 +13,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+export default app
 
