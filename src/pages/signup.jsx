d7a1789d6b0ebd782467425f9b51b38b343a035f
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-30 21:14:19 -0400

    add: sign up page contents

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,13 +1,23 @@
-import { Flex } from '@chakra-ui/react'
-import React from 'react'
-import Header from '../components/Header'
+import { Button, Flex, Input, Text } from "@chakra-ui/react";
+import React from "react";
+import Header from "../components/Header";
 
 const Signup = () => {
   return (
-      <Flex>
-        <Header />
+    <>
+      <Header />
+      <Flex direction="column" alignItems="center" mt={10}>
+        <Flex direction="column" gap={5}>
+          <Text textAlign="center">Sign Up</Text>
+          <label>Email</label>
+          <Input mt={-3} type="email" />
+          <label>Password</label>
+          <Input mt={-3} type="password" />
+          <Button>Sign Up</Button>
+        </Flex>
       </Flex>
-  )
-}
+    </>
+  );
+};
 
-export default Signup
+export default Signup;
