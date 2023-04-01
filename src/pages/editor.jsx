9e5d9bbfbf3b767801c183f86940c47cd81dae4b
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-01 17:06:16 -0400

    add: main editor page

---
+++ src/pages/editor.jsx
@@ -0,0 +1,12 @@
+import { Flex, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Editor = () => {
+  return (
+    <Flex direction="column">
+      <Text>Editor Page</Text>
+    </Flex>
+  );
+};
+
+export default Editor;
