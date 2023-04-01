60ed60dc38071e806a0b6d21c335e4b5f92e5662
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-01 17:08:03 -0400

    refactor: separate pages for login/ signup

--- src/components/HomepageHero.jsx
+++ src/components/HomepageHero.jsx
@@ -6,52 +6,34 @@ import {
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
-import React, { useState, useContext } from "react";
-import { auth, googleProvider } from "../config/firebase";
-import {
-  createUserWithEmailAndPassword,
-  signInWithPopup,
-  signOut,
-} from "firebase/auth";
-import { AuthContext } from "../config/AuthProvider";
+import React from "react";
+// import { auth, googleProvider } from "../config/firebase";
+// import {
+//   signInWithEmailAndPassword,
+//  signInWithPopup,
+//  signOut,
+//} from "firebase/auth";
+// import { AuthContext } from "../config/AuthProvider";
 import { NavLink, useNavigate } from "react-router-dom";
 
 const HomepageHero = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const { currentUser } = useContext(AuthContext);
-  const navigate = useNavigate();
+  //const signInWithGoogle = async () => {
+  //  try {
+  //kh    await signInWithPopup(auth, googleProvider);
+  //  } catch (err) {
+  //    alert(err.message);
+  //ih   }
+  // };
 
-  const signIn = async () => {
-    try {
-      if (currentUser) {
-        logOut();
-      } else {
-        await createUserWithEmailAndPassword(auth, email, password);
-        // navigate("/mainapp")
-      }
-    } catch (err) {
-      alert(err.message);
-    }
-  };
-
-  const signInWithGoogle = async () => {
-    try {
-      await signInWithPopup(auth, googleProvider);
-    } catch (err) {
-      alert(err.message);
-    }
-  };
-
-  const logOut = async () => {
-    try {
-      await signOut(auth);
-      alert("Logged Out Successfully");
-      navigate("/");
-    } catch (err) {
-      alert(err.message);
-    }
-  };
+  // const logOut = async () => {
+  // try {
+  //   await signOut(auth);
+  //   alert("Logged Out Successfully");
+  //   navigate("/");
+  // } catch (err) {
+  //  alert(err.message);
+  // }
+  //};
 
   return (
     <Flex
@@ -66,41 +48,12 @@ const HomepageHero = () => {
       </Flex>
       <Divider orientation="vertical" />
       <Flex direction="column" gap={5}>
-        {currentUser ? (
-          <Text>Welcome!</Text>
-        ) : (
-          <>
-            <Text mt={9} textAlign="center">
-              Sign In
-            </Text>
-            <label>Email</label>
-            <Input
-              type="email"
-              errorBorderColor="red.300"
-              placeholder="example@example.com"
-              onChange={(e) => setEmail(e.target.value)}
-              mt={-3}
-              required
-            />
-            <label mt={5}>Password</label>
-            <Input
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-              mt={-3}
-              required
-            />
-
-            <Button onClick={signIn}>
-              {currentUser ? "Log Out" : "Sign In"}
-            </Button>
-            <Button colorScheme="blue" onClick={signInWithGoogle}>
-              Sign In With Google
-            </Button>
-            <Text fontSize="xs" color="gray.400">
-              <NavLink to="/signup">Need an account? Sign Up!</NavLink>
-            </Text>
-          </>
-        )}
+        <Button>
+          <NavLink to="/signin">Log In</NavLink>
+        </Button>
+        <Button>
+          <NavLink to="/signup">Sign Up</NavLink>
+        </Button>
       </Flex>
     </Flex>
   );
