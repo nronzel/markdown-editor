d4e1d9837bf9ad66bcb77b82594af7d8cf50ae7c
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-01 17:09:20 -0400

    refactor: moved logic to app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,66 @@
 import Home from "../pages/home.jsx";
 import Signup from "../pages/signup.jsx";
-import React from "react";
+import Signin from "../pages/signin.jsx";
+import React, { useState, useEffect } from "react";
 import { Flex } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "../config/AuthProvider.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { auth } from "../config/firebase.js";
+import { onAuthStateChanged } from "firebase/auth";
+import PrivateRoute from "./PrivateRoute.jsx";
+import Editor from "../pages/editor.jsx";
+import VerifyEmail from "./VerifyEmail.jsx";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [timeActive, setTimeActive] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+  }, []);
+
   return (
-    <AuthProvider>
-      <Flex direction="column">
-        <BrowserRouter>
+    <Flex direction="column">
+      <BrowserRouter>
+        <AuthProvider value={{ currentUser, timeActive, setTimeActive }}>
           <Routes>
-            <Route path="/" index element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route exact path="/" index element={<Home />} />
+            <Route
+              path="/signup"
+              element={
+                !currentUser?.emailVerified ? (
+                  <Signup />
+                ) : (
+                  <Navigate to="/editor" replace />
+                )
+              }
+            />
+            <Route
+              path="/signin"
+              element={
+                !currentUser?.emailVerified ? (
+                  <Signin />
+                ) : (
+                  <Navigate to="/editor" replace />
+                )
+              }
+            />
+
+            <Route
+              path="/editor"
+              element={
+                <PrivateRoute>
+                  <Editor />
+                </PrivateRoute>
+              }
+            />
+            <Route path="verify-email" element={<VerifyEmail />} />
           </Routes>
-        </BrowserRouter>
-      </Flex>
-    </AuthProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </Flex>
   );
 }
 
