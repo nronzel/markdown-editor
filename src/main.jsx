69b199c2121855508fc5194b3e852b285b4003f7
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-01 17:07:01 -0400

    add: semicolon

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import theme from "./theme.js"
+import theme from "./theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
