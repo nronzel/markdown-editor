66c97c9dbb9fcf44193a0c9cb135667786d4f3aa
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-02 13:27:32 -0400

    add: anonymous login

--- src/components/HomepageHero.jsx
+++ src/components/HomepageHero.jsx
@@ -7,8 +7,22 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { signInAnonymously } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const HomepageHero = () => {
+  const navigate = useNavigate();
+
+  const anonSignIn = async () => {
+    try {
+      await signInAnonymously(auth);
+      navigate("/editor");
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
   return (
     <Flex
       justifyContent="space-evenly"
@@ -19,8 +33,17 @@ const HomepageHero = () => {
       borderColor={useColorModeValue("gray.300", "gray.600")}
       wrap="wrap"
     >
-      <Flex alignItems="center" justifyContent="center" h="100%">
+      <Flex
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        h="100%"
+        gap={5}
+      >
         <Text>Edit and save Markdown files right in your browser!</Text>
+        <Button colorScheme="teal" onClick={anonSignIn}>
+          Try it!
+        </Button>
       </Flex>
       <Flex direction="column" gap={5} alignItems="center">
         <VStack w="100%" gap={5}>
