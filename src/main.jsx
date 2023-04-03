90b5c181d40f3643621e2bd2c347827dffdcff24
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-03 12:48:41 -0400

    add: theme to Chakraprovider

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import theme from "./theme.js";
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
