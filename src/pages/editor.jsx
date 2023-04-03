7b84ff2c5b9c4d9ead8887c672a584141f858df1
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-03 19:17:31 -0400

    add: react markdown component

--- src/pages/editor.jsx
+++ src/pages/editor.jsx
@@ -1,12 +1,16 @@
 import { Flex, Text } from "@chakra-ui/react";
 import React from "react";
 import Header from "../components/Header";
+import MdownPreview from "../components/MdownPreview";
 
 const Editor = () => {
   return (
     <Flex direction="column">
       <Header />
       <Text>Editor Page</Text>
+      <Flex>
+        <MdownPreview />
+      </Flex>
     </Flex>
   );
 };
