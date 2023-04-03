d169d59be81c244fa743aa16df43b90afe831177
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-02 21:16:25 -0400

    refactor: take initial user for testing

--- src/config/AuthProvider.jsx
+++ src/config/AuthProvider.jsx
@@ -1,11 +1,25 @@
-import React, { useContext } from "react";
+import React, { useState, createContext, useContext, useEffect } from "react";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
-const AuthContext = React.createContext();
+export const AuthContext = createContext();
 
-export const AuthProvider = ({ children, value }) => {
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+export const AuthProvider = ({ initialUser, children }) => {
+  const [currentUser, setCurrentUser] = useState(initialUser);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setCurrentUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
-export const useAuthValue = () => {
-  return useContext(AuthContext);
-};
+export const useAuthValue = () => useContext(AuthContext);
