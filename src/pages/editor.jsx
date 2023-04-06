133b51455da6e5297e7eee84549e67edb4bc0552
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-05 22:04:17 -0400

    removed unused import

--- src/pages/editor.jsx
+++ src/pages/editor.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import MarkdownEditor from "../components/MarkdownEditor";
-import Header from "../components/Header";
 
 const Editor = () => {
   return <MarkdownEditor />;
