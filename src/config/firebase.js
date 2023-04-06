1afde871a31cd93b727885328bd49f7ece8cd598
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-06 12:55:56 -0400

    regenerated API keys, put new keys in env variables
    I understand the old keys will remain in git history, so I generated new
    keys and added them via environment variables.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7YlbA50Vl5Qi8Qmd-LpRxwijmn7f8z2o",
-  authDomain: "markdown-editor-48193.firebaseapp.com",
-  projectId: "markdown-editor-48193",
-  storageBucket: "markdown-editor-48193.appspot.com",
-  messagingSenderId: "823260975141",
-  appId: "1:823260975141:web:64d583352c28aebdc5afcc"
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId:  `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket:  `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
 export default app
