1a4761ad955c275ac027a7914c62b4e801ffa67e
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-07 19:02:23 -0400

    add: database storage, encryption password check

--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -1,13 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import "../styles/editor.css";
 import Header from "./Header";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../config/firebase";
+import CryptoJS from "crypto-js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
+import DocumentDrawer from "./DocumentDrawer";
 
-const MarkdownEditor = () => {
+const MarkdownEditor = ({ currentUser, encryptionPassword }) => {
   const [markdown, setMarkdown] = useState("");
+  const [showDrawer, setShowDrawer] = useState(false);
+  const drawerRef = useRef(null);
+
+  const toggleDrawer = () => {
+    setShowDrawer((prevShowDrawer) => !prevShowDrawer);
+  };
+
+  const handleOutsideClick = useCallback(
+    (e) => {
+      if (
+        showDrawer &&
+        drawerRef.current &&
+        !drawerRef.current.contains(e.target)
+      ) {
+        toggleDrawer();
+      }
+    },
+    [showDrawer, toggleDrawer]
+  );
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [handleOutsideClick]);
 
   const handleTabKey = (e) => {
     if (e.key === "Tab") {
@@ -26,9 +58,45 @@ const MarkdownEditor = () => {
     }
   };
 
+  const saveDocument = async () => {
+    if (!currentUser) {
+      alert("You must be signed in to save a document.");
+      return;
+    }
+
+    try {
+      const key = encryptionPassword;
+      const encryptedText = CryptoJS.AES.encrypt(markdown, key).toString();
+
+      const docRef = await addDoc(collection(db, "documents"), {
+        content: encryptedText,
+        createdAt: new Date(),
+        uid: currentUser.uid,
+      });
+      console.log("Document saved with ID: ", docRef.id);
+      alert("Document saved successfully.");
+    } catch (err) {
+      console.error("Error saving document: ", err);
+      alert("Failed to saved document.", err.message);
+    }
+  };
+
   return (
     <div className="editor-container">
       <Header />
+      <div className="drawer-btn-container">
+        <FontAwesomeIcon
+          icon={faBars}
+          className="drawer-icon"
+          onClick={toggleDrawer}
+        />
+        <button className="save-btn" onClick={saveDocument}>
+        <FontAwesomeIcon icon={faFloppyDisk} />
+      </button>
+      </div>
+      {showDrawer && (
+        <DocumentDrawer toggleDrawer={toggleDrawer} ref={drawerRef} />
+      )}
       <div className="main-section">
         <div className="container">
           <h2 className="titles">Editor</h2>
