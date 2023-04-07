ae0c969e4a5f1d436298a5e238acd9fb4ebf6796
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-07 19:02:05 -0400

    add: encryption password check

--- src/pages/editor.jsx
+++ src/pages/editor.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import MarkdownEditor from "../components/MarkdownEditor";
+import { AuthContext } from "../config/AuthProvider";
+import { useEncryptionPasswordValue } from "../config/EncryptionWrapper";
+import EncryptionModal from "../components/EncryptionModal.jsx";
 
 const Editor = () => {
-  return <MarkdownEditor />;
+  const { currentUser } = useContext(AuthContext);
+  const { encryptionPassword } = useEncryptionPasswordValue();
+
+  return (
+    <>
+      {!encryptionPassword && <EncryptionModal />}
+      <MarkdownEditor
+        currentUser={currentUser}
+        encryptionPassword={encryptionPassword}
+      />
+    </>
+  );
 };
 
 export default Editor;
