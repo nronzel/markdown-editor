fc57a0f3012756c3fe44aaa009c4646af5b78cf1
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-08 13:40:09 -0400

    add: document name and render users documents

--- src/components/DocumentDrawer.jsx
+++ src/components/DocumentDrawer.jsx
@@ -1,21 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/documentdrawer.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSquareMinus } from "@fortawesome/free-solid-svg-icons";
+import { fromByteArray } from "base64-js";
 
-const DocumentDrawer = React.forwardRef(({ toggleDrawer }, ref) => {
-  return (
-    <div className="main-container" ref={ref}>
-      <div className="drawer-heading">
-        <h2 style={{color: "#08dbaa"}}>Documents</h2>
-        <FontAwesomeIcon
-          className="close-btn"
-          icon={faSquareMinus}
-          onClick={toggleDrawer}
-        />
+const DocumentDrawer = React.forwardRef(
+  ({ toggleDrawer, userDocuments, encryptionKey }, ref) => {
+    const [decryptedDocuments, setDecryptedDocuments] = useState([]);
+
+    useEffect(() => {
+      async function decryptDocuments() {
+        const decryptedDocs = [];
+        console.log("Fetched Docs", userDocuments);
+        for (const doc of userDocuments) {
+          console.log("Current Doc:", doc);
+          console.log("data object:", doc.data);
+          try {
+            const encryptedText = base64ToArrayBuffer(doc.data.content); // Change from doc.data.content
+            const iv = new Uint8Array(
+              doc.data.iv.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)) // Change from doc.data.iv
+            );
+            const decryptedContent = await crypto.subtle.decrypt(
+              { name: "AES-GCM", iv },
+              encryptionKey,
+              encryptedText
+            );
+            const decoder = new TextDecoder();
+            const plaintext = decoder.decode(decryptedContent);
+            decryptedDocs.push({
+              id: doc.id,
+              content: plaintext,
+              name: doc.data.name,
+            });
+          } catch (error) {
+            console.error("Error decrypting document with ID:", doc.id, error);
+          }
+        }
+        setDecryptedDocuments(decryptedDocs);
+      }
+      if (userDocuments.length > 0 && encryptionKey) {
+        decryptDocuments();
+      }
+    }, [userDocuments, encryptionKey]);
+
+    function base64ToArrayBuffer(base64) {
+      // Sanitize the base64 string
+      const sanitizedBase64 = base64
+        .replace(/[\s\r\n]+$/, "")
+        .replace(/[^A-Za-z0-9+/]/g, "");
+
+      // Use the sanitized base64 string
+      const binaryString = window.atob(sanitizedBase64);
+      const bytes = new Uint8Array(binaryString.length);
+      for (let i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+      }
+      return bytes.buffer;
+    }
+
+    return (
+      <div className="main-container" ref={ref}>
+        <div className="drawer-header">
+          <h3>Saved Documents</h3>
+        </div>
+        <ul>
+          {decryptedDocuments.map((doc) => (
+            <li key={doc.id}>{doc.name}</li>
+          ))}
+        </ul>
+        <button onClick={toggleDrawer}>Close</button>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default DocumentDrawer;
