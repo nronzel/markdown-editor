d322c3db91d2a52a4e8bfdb1e59dabf08a51caf9
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-07 21:10:16 -0400

    removed cryptoJS - use Web Crypto API instead
    realized that CryptoJS is no longer maintained, so I opted to use the
    Web Crypto API instead

--- src/config/EncryptionWrapper.jsx
+++ src/config/EncryptionWrapper.jsx
@@ -1,29 +1,50 @@
 import React, { useState, createContext, useContext } from "react";
-import CryptoJS from "crypto-js";
 
 export const EncryptionPasswordContext = createContext();
 
 export const EncryptionPasswordProvider = ({ children }) => {
-  const [encryptionPassword, setEncryptionPassword] = useState(null);
+  const [encryptionKey, setEncryptionKey] = useState(null);
+  const [isCorrectPassword, setIsCorrectPassword] = useState(false);
 
-  function hashPassword(password) {
-    const salt = CryptoJS.lib.WordArray.random(128 / 8);
-    const key = CryptoJS.PBKDF2(password, salt, {
-      keySize: 256 / 32,
-      iterations: 1000,
-    }).toString();
-    return key;
+  async function deriveKey(password, salt) {
+    const encoder = new TextEncoder();
+    const passwordBuffer = encoder.encode(password);
+    const saltBuffer = new Uint8Array(
+      salt.match(/.{1,2}/g).map((byte) => parseInt(byte, 16))
+    );
+    const subtle = window.crypto.subtle;
+    const key = await subtle.importKey("raw", passwordBuffer, "PBKDF2", false, [
+      "deriveKey",
+    ]);
+    const derivedKey = await subtle.deriveKey(
+      {
+        name: "PBKDF2",
+        salt: saltBuffer,
+        iterations: 1000,
+        hash: "SHA-256",
+      },
+      key,
+      { name: "AES-GCM", length: 256 },
+      false,
+      ["encrypt", "decrypt"]
+    );
+    return derivedKey;
   }
 
-  function handleEncryptionKeyEntered(password) {
-    const hashedPassword = hashPassword(password);
-    setEncryptionPassword(hashedPassword);
-    console.log(hashedPassword);
+  async function handleEncryptionKeyEntered(password, salt) {
+    const derivedKey = await deriveKey(password, salt);
+    setEncryptionKey(derivedKey);
+    setIsCorrectPassword(true);
   }
 
   return (
     <EncryptionPasswordContext.Provider
-      value={{ encryptionPassword, handleEncryptionKeyEntered }}
+      value={{
+        encryptionKey,
+        handleEncryptionKeyEntered,
+        isCorrectPassword,
+        setIsCorrectPassword,
+      }}
     >
       {children}
     </EncryptionPasswordContext.Provider>
