f41ded926a33c8778ee0e6d834148669bf29bc8f
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-07 21:11:46 -0400

    fix: encryption salt storage and retrieval

--- src/components/EncryptionModal.jsx
+++ src/components/EncryptionModal.jsx
@@ -1,44 +1,85 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { EncryptionPasswordContext } from "../config/EncryptionWrapper.jsx";
 import "../styles/encryptionmodal.css";
-import { signOut } from "firebase/auth";
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { auth, db } from "../config/firebase";
 
 const MAX_ATTEMPTS = 6;
 
 const EncryptionModal = () => {
   const [encryptionPassword, setEncryptionPassword] = useState("");
   const [numAttempts, setNumAttempts] = useState(0);
-  const { handleEncryptionKeyEntered } = useContext(EncryptionPasswordContext);
+  const [userSalt, setUserSalt] = useState(null);
+  const { handleEncryptionKeyEntered, isCorrectPassword } = useContext(
+    EncryptionPasswordContext
+  );
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        fetchUserSalt(user.uid);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const fetchUserSalt = async (uid) => {
+    try {
+      const userDocRef = doc(db, "userSalts", uid);
+      const userDocSnap = await getDoc(userDocRef);
+      if (userDocSnap.exists()) {
+        setUserSalt(userDocSnap.data().salt);
+      } else {
+        const newSalt = generateRandomSalt();
+        await setDoc(userDocRef, { salt: newSalt });
+        setUserSalt(newSalt);
+      }
+    } catch (error) {
+      console.error("Error fetching user salt:", error);
+    }
+  };
+
+  const generateRandomSalt = () => {
+    const array = new Uint8Array(16);
+    crypto.getRandomValues(array);
+    return Array.from(array)
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join("");
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (numAttempts >= MAX_ATTEMPTS) {
-      signOut();
+      signOut(auth);
     } else {
-      handleEncryptionKeyEntered(encryptionPassword);
+      handleEncryptionKeyEntered(encryptionPassword, userSalt);
       setNumAttempts(numAttempts + 1);
     }
   };
 
   return (
     <>
-      <div className="modal-container">
-        <div className="modal">
-          <p>Please enter your encryption password</p>
-          <input
-            onChange={(e) => setEncryptionPassword(e.target.value)}
-            value={encryptionPassword}
-            type="password"
-            required
-          />
-          <button className="btn" onClick={handleSubmit}>
-            Submit
-          </button>
+      {!isCorrectPassword && (
+        <div className="modal-container">
+          <div className="modal">
+            <p>Please enter your encryption password</p>
+            <input
+              onChange={(e) => setEncryptionPassword(e.target.value)}
+              value={encryptionPassword}
+              type="password"
+              required
+            />
+            <button className="btn" onClick={handleSubmit} disabled={!userSalt}>
+              Submit
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="overlay"></div>
+      )}
+      {!isCorrectPassword && <div className="overlay"></div>}
     </>
   );
 };
-
 export default EncryptionModal;
