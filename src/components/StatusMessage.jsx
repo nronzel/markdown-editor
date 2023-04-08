6c7c20b0db5f04787dd3e2d96b69c448700bb7ec
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-07 21:09:07 -0400

    add: component to display status message

---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import "../styles/statusmessage.css"
+
+const StatusMessage = ({ statusMessage, clearStatusMessage }) => {
+  const { type, message } = statusMessage;
+  const messageClass = type === "success" ? "success-message" : "error-message";
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      clearStatusMessage();
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [statusMessage, clearStatusMessage]);
+
+  return <div className={messageClass}>{message}</div>;
+};
+
+export default StatusMessage;
