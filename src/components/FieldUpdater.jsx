bc268fe706ca0c6201246ead9c03ccdc98d9bfe4
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-10 14:36:14 -0400

    add: field updater component

---
+++ src/components/FieldUpdater.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "../styles/profile.css"
+
+const FieldUpdater = ({
+  label,
+  fieldType,
+  initialValue,
+  onUpdate,
+  isEditing,
+  setIsEditing,
+}) => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await onUpdate(value);
+      setIsEditing(false);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return isEditing ? (
+    <form onSubmit={handleSubmit} className="field">
+      <div className="field">
+        <label>{label}</label>
+        <input
+          type={fieldType}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button className="profile-btn" type="submit">
+          Save
+        </button>
+      </div>
+      <button className="regular-btn" onClick={() => setIsEditing(false)}>
+        Go Back
+      </button>
+    </form>
+  ) : (
+    <div className="field">
+      <p>
+        {label}: {initialValue}
+      </p>
+      <button className="profile-btn" onClick={() => setIsEditing(true)}>
+        Change
+      </button>
+    </div>
+  );
+};
+
+export default FieldUpdater;
