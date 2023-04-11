51757991c443c083359a1709dcba8edc12052969
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-11 17:05:52 -0400

    fixed route when anonymous
    when trying to sign up as an anonymous user, it would bring you to the
    verify email page, rather than the sign up page. This change fixes this.

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
               <Route
                 path="/signup"
                 element={
-                  !currentUser ? (
+                  !currentUser || currentUser.isAnonymous ? (
                     <Signup />
                   ) : currentUser.emailVerified ? (
                     <Navigate to="/editor" replace />
