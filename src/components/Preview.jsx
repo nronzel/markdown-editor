1a31542b624378ae622adc24c2a1c413e4fc1367
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-12 15:58:26 -0400

    modified style; added wrap-text option

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import "../styles/editor.css";
 
 const Preview = ({ markdown }) => {
+  const [textWrap, setTextWrap] = useState(false);
+
+  const handleTextWrap = (e) => {
+    setTextWrap(e.target.checked);
+  };
+
   return (
     <div className="container">
-      <h2 className="titles">Preview</h2>
+      <div className="preview-head">
+        <h2 className="titles">Preview</h2>
+        <div className="flex">
+          <input
+            className="wrap-text"
+            type="checkbox"
+            value="textWrap"
+            onChange={handleTextWrap}
+          />{" "}
+          <p className="small-txt">Wrap Text?</p>
+        </div>
+      </div>
       <ReactMarkdown
         children={markdown}
         className="preview"
@@ -20,6 +38,7 @@ const Preview = ({ markdown }) => {
                 children={String(children).replace(/\n$/, "")}
                 style={coldarkDark}
                 showLineNumbers="true"
+                wrapLongLines={textWrap}
                 language={match[1]}
                 PreTag="div"
                 {...props}
